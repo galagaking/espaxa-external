828b07cc2e542184b8775b8a95de642ccf78be2e
galagaking <frank@beksonline.nl>
2025-09-04 22:12:43 +0200

    Update __init__.py
    New schema helper

--- components/espaxa/cover/__init__.py
+++ components/espaxa/cover/__init__.py
@@ -1,21 +1,20 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import cover
-
+from esphome.const import CONF_ID
 from .. import espaxa_ns, EspAxa
 
 EspAxaCover = espaxa_ns.class_("EspAxaCover", cover.Cover)
 CONF_ESPAXA_ID = "espaxa_id"
 
-CONFIG_SCHEMA = cover.COVER_SCHEMA.extend(
+CONFIG_SCHEMA = cover.cover_schema(EspAxaCover).extend(
     {
-        cv.GenerateID(): cv.declare_id(EspAxaCover),
         cv.GenerateID(CONF_ESPAXA_ID): cv.use_id(EspAxa),
     }
 )
 
 async def to_code(config):
     parent = await cg.get_variable(config[CONF_ESPAXA_ID])
-    var = cg.new_Pvariable(config["id"])
+    var = cg.new_Pvariable(config[CONF_ID])
     await cover.register_cover(var, config)
     cg.add(var.set_parent(parent))
