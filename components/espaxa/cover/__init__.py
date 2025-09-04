dddcdee3bb4d071932a45acd6b21215698012c9c
galagaking <frank@beksonline.nl>
2025-09-04 22:13:54 +0200

    Update __init__.py
    New schema helper

--- components/espaxa/cover/__init__.py
+++ components/espaxa/cover/__init__.py
@@ -1,13 +1,13 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import cover
+from esphome.components import sensor
 from esphome.const import CONF_ID
 from .. import espaxa_ns, EspAxa
 
-EspAxaCover = espaxa_ns.class_("EspAxaCover", cover.Cover)
+EspAxaStatusSensor = espaxa_ns.class_("EspAxaStatusSensor", sensor.Sensor)
 CONF_ESPAXA_ID = "espaxa_id"
 
-CONFIG_SCHEMA = cover.cover_schema(EspAxaCover).extend(
+CONFIG_SCHEMA = sensor.sensor_schema(EspAxaStatusSensor).extend(
     {
         cv.GenerateID(CONF_ESPAXA_ID): cv.use_id(EspAxa),
     }
@@ -16,5 +16,5 @@ CONFIG_SCHEMA = cover.cover_schema(EspAxaCover).extend(
 async def to_code(config):
     parent = await cg.get_variable(config[CONF_ESPAXA_ID])
     var = cg.new_Pvariable(config[CONF_ID])
-    await cover.register_cover(var, config)
-    cg.add(var.set_parent(parent))
+    await sensor.register_sensor(var, config)
+    # cg.add(var.set_parent(parent))  # if you add set_parent on the C++ side
