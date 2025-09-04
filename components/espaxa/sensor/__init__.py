5f1f7527cc550915d5a96968d0b1a76e9590cd12
galagaking <frank@beksonline.nl>
2025-09-04 22:26:34 +0200

    Update __init__.py

--- components/espaxa/sensor/__init__.py
+++ components/espaxa/sensor/__init__.py
@@ -1,21 +1,25 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
+from esphome.const import CONF_ID
 
 from .. import espaxa_ns, EspAxa
 
-EspAxaStatusSensor = espaxa_ns.class_("EspAxaStatusSensor", sensor.Sensor,cg.Component)
+# C++ class (Sensor + Component)
+EspAxaStatusSensor = espaxa_ns.class_("EspAxaStatusSensor", sensor.Sensor, cg.Component)
+
 CONF_ESPAXA_ID = "espaxa_id"
 
-CONFIG_SCHEMA = sensor.SENSOR_SCHEMA.extend(
+# Use the new schema helper (COVER_SCHEMA/SENSOR_SCHEMA are deprecated)
+CONFIG_SCHEMA = sensor.sensor_schema(EspAxaStatusSensor).extend(
     {
-        cv.GenerateID(): cv.declare_id(EspAxaStatusSensor),
         cv.GenerateID(CONF_ESPAXA_ID): cv.use_id(EspAxa),
     }
 )
 
 async def to_code(config):
     parent = await cg.get_variable(config[CONF_ESPAXA_ID])
-    var = cg.new_Pvariable(config["id"])
+    var = cg.new_Pvariable(config[CONF_ID])
     await sensor.register_sensor(var, config)
-    # cg.add(var.set_parent(parent)) if you implement set_parent
+    await cg.register_component(var, {})  # since C++ inherits Component
+    cg.add(var.set_parent(parent))
