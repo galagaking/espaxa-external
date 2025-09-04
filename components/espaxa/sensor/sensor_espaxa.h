264ce2602c71dd72d0f9ceef702f8b295ecd51d4
galagaking <frank@beksonline.nl>
2025-09-04 22:27:23 +0200

    Update sensor_espaxa.h

--- components/espaxa/sensor/sensor_espaxa.h
+++ components/espaxa/sensor/sensor_espaxa.h
@@ -1,5 +1,7 @@
 #pragma once
+
 #include "esphome/components/sensor/sensor.h"
+#include "esphome/core/log.h"
 #include "../espaxa.h"
 
 namespace espaxa {
@@ -7,13 +9,24 @@ namespace espaxa {
 class EspAxaStatusSensor : public esphome::sensor::Sensor, public esphome::Component {
  public:
   void set_parent(EspAxa *p) { parent_ = p; }
-  void setup() override {}
-  void loop() override {
-    int s = parent_->read_status();
-    publish_state(s); // enable when needed
+
+  void setup() override {
+    // nothing to init
   }
+
+  void loop() override {
+    if (parent_ == nullptr) return;
+    const int s = parent_->read_status();
+    if (s != last_published_) {
+      // Publish raw status code (e.g., 210/211/200/502)
+      this->publish_state(static_cast<float>(s));
+      last_published_ = s;
+    }
+  }
+
  private:
-  EspAxa *parent_{nullptr};
+    EspAxa *parent_{nullptr};
+    int last_published_{INT32_MIN};
 };
 
 }  // namespace espaxa
